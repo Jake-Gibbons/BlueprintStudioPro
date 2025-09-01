1e3de664f719eaf2ad1ab121f4b5e3e5da571ae5
Jake Gibbons <jake_gibbons@live.co.uk>
2025-09-01 20:30:21 +0100

    Initial Commit

---
+++ BlueprintStudioPro/BlueprintStudioProApp.swift
@@ -0,0 +1,32 @@
+//
+//  BlueprintStudioProApp.swift
+//  BlueprintStudioPro
+//
+//  Created by Jake Gibbons on 01/09/2025.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct BlueprintStudioProApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ BlueprintStudioProTests/BlueprintStudioProTests.swift
@@ -0,0 +1,17 @@
+//
+//  BlueprintStudioProTests.swift
+//  BlueprintStudioProTests
+//
+//  Created by Jake Gibbons on 01/09/2025.
+//
+
+import Testing
+@testable import BlueprintStudioPro
+
+struct BlueprintStudioProTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
